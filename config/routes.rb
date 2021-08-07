6a185bc7275de45396568849c437885b505a115b
lflores1961 <fernando.flores1961@gmail.com>
2021-08-06 21:32:15 -0500

    start(application): Create landing page
    Create the starting point landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'welcome/start'
+  root to: "welcome#start"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
